91769fd2dd03d8d30be70240218ff3fd8a4afe2d
Gustavo <gustavoeqr1017@gmail.com>
2020-08-18 20:32:20 -0500

    avance modal

--- src/app/acreditacion/documento/documento.component.ts
+++ src/app/acreditacion/documento/documento.component.ts
@@ -8,22 +8,41 @@ import { Component, OnInit } from '@angular/core';
 export class DocumentoComponent implements OnInit {
  
   public iconoFlechaMostrarMotivoTramite: String;
+  public iconoFlechaMostrarDatosSolicitud: String;
+  public iconoFlechaMostrarDatosAfiliado: String;
   
   constructor() {
 
-    this.iconoFlechaMostrarMotivoTramite = "fas fa-angle-down"
+    this.iconoFlechaMostrarMotivoTramite = "fas fa-angle-up"
+    this.iconoFlechaMostrarDatosSolicitud = "fas fa-angle-up"
+    this.iconoFlechaMostrarDatosAfiliado = "fas fa-angle-up"
    }
 
   ngOnInit(): void {
   }
 
   mostrarMotivoTramite(){
-    if(this.iconoFlechaMostrarMotivoTramite.substr(this.iconoFlechaMostrarMotivoTramite.length-2,this.iconoFlechaMostrarMotivoTramite.length) === 'up' ){
-      this.iconoFlechaMostrarMotivoTramite = "fas fa-angle-down"
-    }else{
+    if(this.iconoFlechaMostrarMotivoTramite.substr(this.iconoFlechaMostrarMotivoTramite.length-2,this.iconoFlechaMostrarMotivoTramite.length) !== 'up' ){
       this.iconoFlechaMostrarMotivoTramite = "fas fa-angle-up"
+    }else{
+      this.iconoFlechaMostrarMotivoTramite = "fas fa-angle-down"
     }
-    
+  }
+
+  mostrarDatosSolicitud(){
+    if(this.iconoFlechaMostrarDatosSolicitud.substr(this.iconoFlechaMostrarDatosSolicitud.length-2,this.iconoFlechaMostrarDatosSolicitud.length) !== 'up' ){
+      this.iconoFlechaMostrarDatosSolicitud = "fas fa-angle-up"
+    }else{
+      this.iconoFlechaMostrarDatosSolicitud = "fas fa-angle-down"
+    }
+  }
+
+  mostrarDatosAfiliado(){
+    if(this.iconoFlechaMostrarDatosAfiliado.substr(this.iconoFlechaMostrarDatosAfiliado.length-2,this.iconoFlechaMostrarDatosAfiliado.length) !== 'up' ){
+      this.iconoFlechaMostrarDatosAfiliado = "fas fa-angle-up"
+    }else{
+      this.iconoFlechaMostrarDatosAfiliado = "fas fa-angle-down"
+    }
   }
 
 }
